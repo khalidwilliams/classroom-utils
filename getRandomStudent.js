2115ce3e36ce40b193ea0c66652e7306e0e78dd4
Khalid Williams <khalidwill27@gmail.com>
2020-02-03 16:03:41 -0700

    Clean up code

--- getRandomStudent.js
+++ getRandomStudent.js
@@ -1,7 +1,5 @@
 const students = require('./current-students.js');
 
-console.log(students);
-
 function fyShuffle (array) {
   for (let i = array.length; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
