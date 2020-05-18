58196743e0fe41f41ce889c1ce6387664794ab61
Khalid Williams <khalidwill27@gmail.com>
2020-05-17 21:51:13 -0600

    Prevent duplication in getRandomStudents

--- getRandomStudent.js
+++ getRandomStudent.js
@@ -1,5 +1,29 @@
 const shuffledStudents = require('./shuffle.js');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+async function printAndAwait (answer) {
+  await popAndPrint();
+}
+
+function popAndPrint () {
+  // print student
+  let randomStudent = shuffledStudents.pop();
+  console.log(randomStudent);
+  if (!shuffledStudents.length) {
+    rl.close();
+    return Promise.resolve('No more students');
+  } else {
+    rl.question('Hit enter for a new student', printAndAwait)
+  }
+}
 
 
-let randomStudent = shuffledStudents.pop();
-console.log(randomStudent);
+
+
+rl.question('Hit enter for a new student', printAndAwait);
+
